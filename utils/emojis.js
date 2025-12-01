539b9e729e0498d9814f25b6f0943b838c43d3dc
Davenads <davidnadler9637@gmail.com>
2025-11-30 20:37:22 -0500

    Update emojis to use production server IDs

--- utils/emojis.js
+++ utils/emojis.js
@@ -2,11 +2,13 @@
  * Custom Discord emojis for the NvD Bot
  *
  * These emojis represent the two classes in the Necromancer vs Druid ladder:
- * - Necromancer (necro): Represented by a skull/necromancer icon
- * - Druid (druid): Represented by a druid icon
+ * - Necromancer (Necro): Represented by a skull/necromancer icon
+ * - Druid: Represented by a druid icon
+ *
+ * Using production server emojis to ensure compatibility across all guilds
  */
 
 module.exports = {
-  necro: '<:necro:1436845477587980328>',
-  druid: '<:druid:1436845962369826897>'
+  necro: '<:Necro:994817323653419058>',
+  druid: '<:Druid:994817312563671050>'
 };
